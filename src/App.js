d7453ada8c8fdd8ca98f95b535e8f2b23290625f
Jimi van der Woning <jimivdwoning@gmail.com>
2018-03-22 03:57:47 +0100

    feat: add payment flow aka penis glow

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import { styles } from './App.styles';
 import { Menu } from './Menu';
 import { NewWallet } from './NewWallet';
 import { JoinWallet } from './JoinWallet';
+import { Pay } from './Pay';
 
 class App extends Component {
   state = {
     selectedMenu: 'New',
-    isAuthenticated: false
+    isAuthenticated: false,
+    walletId: '-L8AJW3C1gGUOe2lxyYt'
   };
 
-  menuItems = ['New', 'Join', 'Wallets'];
+  menuItems = ['New', 'Join', 'Wallets', 'Pay'];
 
   constructor() {
     super();
@@ -26,6 +28,7 @@ class App extends Component {
 
     this.onWalletCreated = this.onWalletCreated.bind(this);
     this.onWalletJoined = this.onWalletJoined.bind(this);
+    this.onPaid = this.onPaid.bind(this);
   }
 
   changeMenuItem = value => {
@@ -39,6 +42,11 @@ class App extends Component {
 
   onWalletJoined(walletId) {
     console.log('Wallet joined', walletId);
+    this.setState({ selectedMenu: 'Wallet' });
+  }
+
+  onPaid(walletId) {
+    console.log('Wallet paid', walletId);
     this.setState({ selectedMenu: 'Wallet' });
   }
 
@@ -74,6 +82,8 @@ class App extends Component {
         return 'Wallets';
       case 'Wallet':
         return 'Wallet x';
+      case 'Pay':
+        return <Pay onPaid={this.onPaid} walletId={this.state.walletId}/>;
       default:
         return 'Hello';
     }
